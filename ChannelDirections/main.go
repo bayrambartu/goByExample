a8839b2d27df55ac3151bba8365d8801e4dc49b8
bayram bartu <bayram213062@ogr.duzce.edu.tr>
2024-12-13 23:52:41 +0300

    Channel Directions

---
+++ ChannelDirections/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// <- chan t (alma)
+// chan <- t (gönderme)
+// Bu fonksiyon, yalnızca veri gönderen bir kanalı alır.
+// Gönderme yönü: chan<- string (sadece gönderme)
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings // bir kanaldan veri alınır
+	pongs <- msg   // diger kanala veri gönderilir
+}
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+	//fmt.Println(<-pings) --fatal error: all goroutines are asleep - deadlock!
+}
