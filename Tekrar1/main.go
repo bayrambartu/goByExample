b0bbcec6836ed7021df2286ca8acd1fe3ecca50d
bayrambartu <kurnazb32@gmail.com>
2025-01-20 23:53:23 +0300

    u

--- Tekrar1/main.go
+++ Tekrar1/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
 	"time"
 )
 
@@ -74,57 +72,100 @@ func main() {
 		wg.Wait()
 		fmt.Println("done") */
 
-	rand.NewSource(time.Now().UnixNano()) // random data
+	/*rand.NewSource(time.Now().UnixNano()) // random data
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup*/
 
 	// <Gondericiler (goroutine'ler)
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		for i := 0; i < 5; i++ {
-			ch1 <- rand.Intn(100)
-			time.Sleep(time.Millisecond * 100)
+	/*	wg.Add(2)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 5; i++ {
+				ch1 <- rand.Intn(100)
+				time.Sleep(time.Millisecond * 100)
+			}
+		}()
+
+		go func() {
+			defer wg.Done()
+			for i := 0; i < 5; i++ {
+				ch2 <- rand.Intn(100)
+				time.Sleep(time.Millisecond * 100)
+			}
+		}()
+
+		//<Alıcılar(goroutine'ler)
+
+		go func() {
+			wg.Wait()
+			close(ch1)
+			close(ch2)
+		}()
+
+		for {
+			select {
+			case val, ok := <-ch1:
+				if ok {
+					fmt.Println("ch1'den alinan veri:", val)
+
+				} else {
+					ch1 = nil // kanal kapandi artik dinlenmeyecek.!
+				}
+			case val, ok := <-ch2:
+				if ok {
+					fmt.Println("ch2'den alinan veri:", val)
+				} else {
+					ch2 = nil
+				}
+			}
+			if ch1 == nil && ch2 == nil {
+				break
+			}
 		}
-	}()
+		fmt.Println("DONE") */
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
 	go func() {
-		defer wg.Done()
-		for i := 0; i < 5; i++ {
-			ch2 <- rand.Intn(100)
-			time.Sleep(time.Millisecond * 100)
+		for i := 1; i <= 5; i++ {
+			ch1 <- fmt.Sprintf("Kanal 1: Mesaj %d", i)
+			time.Sleep(500 * time.Millisecond) // Daha hızlı gönderim
 		}
-	}()
-
-	//<Alıcılar(goroutine'ler)
-
-	go func() {
-		wg.Wait()
 		close(ch1)
+	}()
+
+	go func() {
+		for i := 1; i <= 3; i++ {
+			ch2 <- fmt.Sprintf("Kanal 2: Mesaj %d", i)
+			time.Sleep(1 * time.Second)
+		}
 		close(ch2)
 	}()
 
 	for {
 		select {
-		case val, ok := <-ch1:
+		case msg1, ok := <-ch1:
 			if ok {
-				fmt.Println("ch1'den alinan veri:", val)
-
+				fmt.Println(msg1)
 			} else {
-				ch1 = nil // kanal kapandi artik dinlenmeyecek.!
+				ch1 = nil
 			}
-		case val, ok := <-ch2:
+		case msg2, ok := <-ch2:
 			if ok {
-				fmt.Println("ch2'den alinan veri:", val)
+				fmt.Println(msg2)
 			} else {
 				ch2 = nil
 			}
 		}
+
 		if ch1 == nil && ch2 == nil {
 			break
 		}
 	}
+
 	fmt.Println("DONE")
+
 }
