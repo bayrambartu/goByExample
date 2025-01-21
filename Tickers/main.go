fc96bf7b47d2bbb0c315db45868a426703b9f5bf
bayrambartu <kurnazb32@gmail.com>
2025-01-22 00:25:43 +0300

    over again
    over again

--- Tickers/main.go
+++ Tickers/main.go
@@ -26,7 +26,7 @@ func main() {
 	done <- true
 	fmt.Println("Ticker stopped") */
 
-	ticker := time.NewTicker(2 * time.Second)
+	/*ticker := time.NewTicker(2 * time.Second)
 	done := make(chan bool)
 	go func() {
 		for {
@@ -41,5 +41,46 @@ func main() {
 	}()
 	time.Sleep(10 * time.Second)
 	done <- true
-	fmt.Println("Ticker stopped.")
+	fmt.Println("Ticker stopped.")*/
+	/*
+		ticker := time.Tick(time.Second * 1)
+		for i := 1; i <= 3; i++ {
+			<-ticker // 1 saniye bekler
+			fmt.Printf("Periyodik işlem %d gerçekleşti\n", i)
+		}
+		fmt.Println("tüm işlemler tamamlandı.")*/
+
+	/*timeout := time.After(3 * time.Second)
+	fmt.Println("beklekniyor.(3sn)")
+	<-timeout
+	fmt.Println("3 saniye bekledikten sonra islem gerceklesti")*/
+
+	/*
+		ticker := time.Tick(1 * time.Second)
+		timeout := time.After(5 * time.Second)
+		fmt.Println("basliyor")
+		for {
+			select {
+			case <-ticker:
+				fmt.Println("Periyodik islem gerceklesti")
+			case <-timeout:
+				fmt.Println("zaman asimi")
+				return
+			}
+		}*/
+
+	// ZAMANLAYICI VE KANALLARI BİRLEŞTİRME
+	ch := make(chan string)
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch <- "kanal uzerinden veri gonderildi"
+	}()
+	timeout := time.After(3 * time.Second)
+	select {
+	case msg := <-ch:
+		fmt.Println(msg)
+	case <-timeout:
+		fmt.Println("Zaman Asimi")
+	}
+
 }
