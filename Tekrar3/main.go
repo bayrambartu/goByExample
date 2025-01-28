eac8261707b7439f0d2c3862eefde515c0e07407
bayrambartu <kurnazb32@gmail.com>
2025-01-28 20:53:13 +0300

    Update main.go

--- Tekrar3/main.go
+++ Tekrar3/main.go
@@ -59,29 +59,62 @@ func main() {
 		}
 	*/
 
-	// Patlama Limiti ve Sıralı Yönetim
+	/*
+		// Patlama Limiti ve Sıralı Yönetim
 
-	requests := make(chan int, 15)
+		requests := make(chan int, 15)
 
-	for i := 1; i <= 15; i++ {
-		requests <- i
+		for i := 1; i <= 15; i++ {
+			requests <- i
+		}
+		close(requests)
+
+		limiter := make(chan time.Time, 5)
+
+		// ilk 5 işlem için tmaponu doldur.
+		for i := 0; i < 5; i++ {
+			limiter <- time.Now()
+		}
+
+		// işlem izini ver.....
+		go func() {
+			for t := range time.Tick(time.Second * 2) {
+				limiter <- t
+			}
+		}()
+
+		// işlmeleri sırayla gerçekleştirdik
+		for req := range requests {
+			<-limiter // tampondan işlem izni al
+			fmt.Printf("İşlem %d gerçekleşti \n", req)
+		}
+	*/
+
+	apiRequest := make(chan int, 20)
+
+	for i := 1; i <= 20; i++ {
+		apiRequest <- i
+
 	}
-	close(requests)
+	close(apiRequest)
 
+	// Burst Limit.
 	limiter := make(chan time.Time, 5)
 
+	// tampon için doldurma....
 	for i := 0; i < 5; i++ {
 		limiter <- time.Now()
 	}
 
+	// her 2 saniyede bir yeni işlem izni ekle
 	go func() {
-		for t := range time.Tick(time.Second * 2) {
+		for t := range time.Tick(2 * time.Second) {
 			limiter <- t
 		}
 	}()
 
-	for req := range requests {
-		<-limiter // tampondan işlem izni al
-		fmt.Printf("İşlem %d gerçekleşti \n", req)
+	for req := range apiRequest {
+		<-limiter // işlem izni almak için bekle
+		fmt.Println("API isteği %d işlendi. Zaman %s \n", req, time.Now().Format("15:04:05"))
 	}
 }
