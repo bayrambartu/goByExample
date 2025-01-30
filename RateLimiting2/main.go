4d3e1aecf23a1d560a9ca4279a26b7a4c670eafe
bayrambartu <kurnazb32@gmail.com>
2025-01-30 23:14:36 +0300

    Rate Limiting 2
    Rate limiting 2

---
+++ RateLimiting2/main.go
@@ -0,0 +1,54 @@
+package main
+
+func main() {
+
+	/*
+		//limiter := time.Tick(1 * time.Second)
+		limiter := time.Tick(500 * time.Millisecond) //  sn'de 2 işlem
+		for i := 1; i <= 5; i++ {
+
+			<-limiter
+			fmt.Printf("işlem %d yapıldı. Zaman : %s \n", i, time.Now().Format("15:04:05"))
+		} */
+
+	/*requests := make(chan int, 10)
+	for i := 1; i <= 10; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := make(chan time.Time, 2)
+
+	for i := 0; i < 2; i++ {
+		limiter <- time.Now()
+	}
+	go func() {
+		for t := range time.Tick(2 * time.Second) {
+			limiter <- t
+			limiter <- t
+		}
+	}()
+	for req := range requests {
+		<-limiter
+		fmt.Printf("İşlem %d yapıldı. Zaman: %s\n", req, time.Now().Format("15:04:05"))
+
+	}*/
+
+	/*
+		requests := make(chan int, 10)
+		for i := 1; i <= 10; i++ {
+			requests <- i
+		}
+		close(requests)
+
+		limiter := time.Tick(time.Second * 1)
+
+		for req := range requests {
+			<-limiter // izin al
+			fmt.Printf("İşlem %d yapıldı. Zaman: %s\n", req, time.Now().Format("15:04:05"))
+
+		}
+
+	*/
+
+}
