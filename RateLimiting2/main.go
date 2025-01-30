a6fea26d4ed476b86eb54005a42848044e30b05d
bayrambartu <kurnazb32@gmail.com>
2025-01-30 23:57:54 +0300

    Update main.go

--- RateLimiting2/main.go
+++ RateLimiting2/main.go
@@ -55,26 +55,55 @@ func main() {
 		}
 
 	*/
-	requests := make(chan int, 10)
-	for i := 1; i <= 10; i++ {
-		requests <- i
-	}
-	close(requests)
+	/*	requests := make(chan int, 10)
+		for i := 1; i <= 10; i++ {
+			requests <- i
+		}
+		close(requests)
 
-	limiter := make(chan time.Time, 3)
+		limiter := make(chan time.Time, 5)
 
-	for i := 0; i < 3; i++ {
-		limiter <- time.Now()
-	}
-
-	go func() {
-		for t := range time.Tick(1 * time.Second) {
-			limiter <- t
+		for i := 0; i < 5; i++ {
+			limiter <- time.Now()
 		}
 
-	}()
-	for req := range requests {
-		<-limiter
-		fmt.Printf("request: %d , time : %s \n", req, time.Now().Format("2006-01-02 15:04:05"))
+		go func() {
+			for t := range time.Tick(1 * time.Second) {
+				limiter <- t
+			}
+
+		}()
+		for req := range requests {
+			<-limiter
+			fmt.Printf("request: %d , time : %s \n", req, time.Now().Format("2006-01-02 15:04:05"))
+		}
+
+	*/
+	package main
+
+	import (
+		"fmt"
+	"time"
+	)
+
+	func main() {
+		requests := make(chan int, 10)
+
+		// 10 işlem ekleyelim
+		for i := 1; i <= 10; i++ {
+			requests <- i
+		}
+		close(requests)
+
+		limiter := time.Tick(3 * time.Second)
+
+		for req := range requests {
+			select {
+			case <-limiter: // İzin varsa işlemi gerçekleştir
+				fmt.Printf("İşlem %d yapıldı. Zaman: %s\n", req, time.Now().Format("15:04:05"))
+			case <-time.After(2 * time.Second): // 2 saniye içinde işlem yapılmazsa zaman aşımı
+				fmt.Printf("İşlem %d zaman aşımına uğradı!\n", req)
+			}
+		}
 	}
 }
