ee993e084bcb7fdec35c044cb0f5f599c9679988
bayrambartu <kurnazb32@gmail.com>
2025-02-02 23:59:13 +0300

    Update main.go

--- StatefulGoroutines/main.go
+++ StatefulGoroutines/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"sync"
 	"sync/atomic"
-	"time"
 )
 
-type readOp struct {
-	key  int      // okunacak anahtar
-	resp chan int // deger döndürmek için kanal
-}
-type writeOp struct {
-	key  int       // güncellenecek olan anahtar
-	val  int       // yeni deger
-	resp chan bool // yazma islemi basarili mi ?
-}
+
+	type readOp struct {
+		key  int      // okunacak anahtar
+		resp chan int // deger döndürmek için kanal
+	}
+
+	type writeOp struct {
+		key  int       // güncellenecek olan anahtar
+		val  int       // yeni deger
+		resp chan bool // yazma islemi basarili mi ?
+	}
+
 
 func main() {
+
 	var readOps uint64
 	var writeOps uint64
 
@@ -69,4 +72,38 @@ func main() {
 
 	writeOpsFinal := atomic.LoadUint64(&writeOps)
 	fmt.Println("writeOps:", writeOpsFinal)
+
+	/*
+		var counter int
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			go func() {
+				for j := 0; j < 1000; j++ {
+					counter++ // DATA RACE
+
+				}
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		fmt.Println("COUNTER ..:", counter) */
+/*
+	var counter int64
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < 1000; j++ {
+				atomic.AddInt64(&counter, 1)
+
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println("COUNTER ..:", counter)
 }
+
+*/
