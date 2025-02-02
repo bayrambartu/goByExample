f183207a3769dbe81ac124b3a14290e13237efbb
bayrambartu <kurnazb32@gmail.com>
2025-02-02 22:50:45 +0300

    update

--- Mutexes/main.go
+++ Mutexes/main.go
@@ -8,37 +8,67 @@ import (
 // amaç container içersinde birden fazla counter'i eşzamanlı olarak güncelleyebilmek.
 // aynı anda çalışan goroutineler bu sayaçları sync.Mutex kullanrak güvenli hale getirmeli.
 // sync.Mutex paylaşılan verilere aynı anda birden fazla goroutin'in erişmesini önleyen bir kilit mekanizmasıdır.
-type Container struct {
-	mu       sync.Mutex
-	counters map[string]int
-}
 
-func (c *Container) inc(name string) {
-	c.mu.Lock()
-	defer c.mu.Unlock() // fonk sonun kilit serbesttir.
-	c.counters[name]++
-	// veirlen anahtarın sayacaını arttırır.
+/*
+	-------------------------------------------------
+
+	type Container struct {
+		mu       sync.Mutex
+		counters map[string]int
+	}
+
+	func (c *Container) inc(name string) {
+		c.mu.Lock()
+		defer c.mu.Unlock() // fonk sonun kilit serbesttir.
+		c.counters[name]++
+		// veirlen anahtarın sayacaını arttırır.
+	}
+
+---------------------------------------------------------
+*/
+var count int
+var mutex sync.Mutex
+
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	mutex.Lock()
+	count++
+	fmt.Println("worker", id, "updated count to ", count)
+	mutex.Unlock()
+
 }
 
 func main() {
-	c := Container{
-		counters: map[string]int{"a": 0, "b": 0},
-	}
 	var wg sync.WaitGroup
-
-	doIncrement := func(name string, n int) {
-		for i := 0; i < n; i++ {
-			c.inc(name)
-		}
-		wg.Done()
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
 	}
-	wg.Add(3)
-	go doIncrement("a", 10000)
-	go doIncrement("a", 10000)
-	go doIncrement("b", 10000)
 	wg.Wait()
-	fmt.Println(c.counters)
+	fmt.Println(count)
 
+	/*
+			-----------------------------------------------------------
+			c := Container{
+				counters: map[string]int{"a": 0, "b": 0},
+			}
+			var wg sync.WaitGroup
+
+			doIncrement := func(name string, n int) {
+				for i := 0; i < n; i++ {
+					c.inc(name)
+				}
+				wg.Done()
+			}
+			wg.Add(3)
+			go doIncrement("a", 10000)
+			go doIncrement("a", 10000)
+			go doIncrement("b", 10000)
+			wg.Wait()
+			fmt.Println(c.counters)
+		 ----------------------------------------------------------------
+	*/
 	/*var ops int
 	wg := sync.WaitGroup{}
 	mu := sync.Mutex{}
