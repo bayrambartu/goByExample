b374e93e0e5e6251ab3b6f95f8165ccb051413e8
bayrambartu <kurnazb32@gmail.com>
2025-02-07 22:49:06 +0300

    Update main.go

--- Sorting/main.go
+++ Sorting/main.go
@@ -11,4 +11,14 @@ func main() {
 	strs := []string{"c", "a", "b"}
 	slices.Sort(strs)
 	fmt.Println("Strings:", strs) // O: Strings: [a b c]
+
+	ints := []int{7, 2, 4}
+	slices.Sort(ints)
+	fmt.Println("Ints:", ints)
+
+	// sıralı mı ? -> IsSorted
+
+	v := slices.IsSorted(ints)
+	fmt.Println("Is sorted?", v)
+
 }
