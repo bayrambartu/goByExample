738f2157bcf8c764c70a5933943b76d81024e39d
bayrambartu <kurnazb32@gmail.com>
2025-02-13 18:46:26 +0300

    updated

--- Defer/main.go
+++ Defer/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+
 	f := createFile("/tmp/defer.txt")
 	defer closeFile(f)
 	writeFile(f)
@@ -29,4 +30,18 @@ func closeFile(f *os.File) {
 	if err != nil {
 		panic(err)
 	}
+
+	/* defer fmt.Println("World")
+		defer fmt.Println("One")
+		defer fmt.Println("Two")
+		fmt.Println("Hello")
+		myDefer()
+
+	}
+	func myDefer() {
+		for i := 0; i < 5; i++ {
+			defer fmt.Print(i)
+
+		}
+	}*/
 }
---
+++ Recover/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func sillySusan() {
+	fmt.Println("silly susan called")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("hackers have been thwarted")
+		}
+	}()
+
+	panickingPeter()
+	fmt.Println("silly susan finished")
+}
+func panickingPeter() {
+	fmt.Println("panicking peter called")
+
+	panic("oh no")
+	fmt.Println("panicking peter finished")
+}
+func main() {
+	fmt.Println("cascading panics")
+	sillySusan()
+	fmt.Println("end of main func")
+	// cascading Panics
+
+	/*
+		// calli a panic
+		fmt.Println("Panic! in the Go App")
+		defer func() {
+			fmt.Println("I am a deferred function")
+		}()
+		//panic("oh no")
+		fmt.Println("end of main")
+	*/
+
+}
