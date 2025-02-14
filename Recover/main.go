cbe46e99b4371c71788a748e66ee967e46437543
bayrambartu <kurnazb32@gmail.com>
2025-02-14 23:56:04 +0300

    updated

--- Recover/main.go
+++ Recover/main.go
@@ -2,27 +2,32 @@ package main
 
 import "fmt"
 
-func sillySusan() {
-	fmt.Println("silly susan called")
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("hackers have been thwarted")
-		}
-	}()
+/*
+	func sillySusan() {
+		fmt.Println("silly susan called")
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("hackers have been thwarted")
+			}
+		}()
 
-	panickingPeter()
-	fmt.Println("silly susan finished")
-}
-func panickingPeter() {
-	fmt.Println("panicking peter called")
+		panickingPeter()
+		fmt.Println("silly susan finished")
+	}
 
-	panic("oh no")
-	fmt.Println("panicking peter finished")
-}
+	func panickingPeter() {
+		fmt.Println("panicking peter called")
+
+		panic("oh no")
+		fmt.Println("panicking peter finished")
+	}
+*/
 func main() {
-	fmt.Println("cascading panics")
-	sillySusan()
-	fmt.Println("end of main func")
+	/*
+		fmt.Println("cascading panics")
+		sillySusan()
+		fmt.Println("end of main func")
+	*/
 	// cascading Panics
 
 	/*
@@ -35,4 +40,15 @@ func main() {
 		fmt.Println("end of main")
 	*/
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recored Error:\n", r)
+		}
+	}()
+	mayPanic()
+	fmt.Println("After mayPanic()")
+
+}
+func mayPanic() {
+	panic("a problem")
 }
