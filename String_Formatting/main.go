1028fbdb0bcc0532e5684196260c0a5a88dd1b5b
bayrambartu <kurnazb32@gmail.com>
2025-02-17 14:34:26 +0300

    Update main.go

--- String_Formatting/main.go
+++ String_Formatting/main.go
@@ -2,21 +2,29 @@ package main
 
 import "fmt"
 
+/*
 type point struct {
 	x, y int
-}
+}*/
 
 func main() {
-	p := point{1, 2}
+	// struct
+
+	/*p := point{1, 2}
 	fmt.Printf(" Normal: %v\n", p)  // struct icerigi
 	fmt.Printf("Detaylı: %+v\n", p) // alan adlari {x:1,y:2}
 	fmt.Printf("Go formatı: %#v\n", p)
-	fmt.Printf("Türü: %T\n", p)
+	fmt.Printf("Türü: %T\n", p)*/
 
 	/*name := "ALice"
 	age := 25
 	fmt.Printf("My name is %s ! age %d \n", name, age)*/
 
-	// struct
+	// boolean,integer binary format
+	fmt.Printf("boolean: %t\n", false)
+	fmt.Printf("Integer: %d\n ", 123)
+	fmt.Printf("Binary: %b\n", 14)
+	fmt.Printf("ASCII code: %c \n", 33)
+	fmt.Printf("Hexadecimal: %x \n", 456)
 
 }
