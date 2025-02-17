fa4fac9dc749e4d52096b08d2ed5fc4f9f455ba0
bayrambartu <kurnazb32@gmail.com>
2025-02-17 15:06:22 +0300

    Update main.go

--- String_Formatting/main.go
+++ String_Formatting/main.go
@@ -36,12 +36,25 @@ func main() {
 		fmt.Printf("Bilimsel format ( kucuk harf) : %e\n", 87.0)
 		fmt.Printf("bilimsel format ( buyuk harf): %E \n", 12340000000.0)
 	*/
+	/*
+		// String formatlama ve Pointer adresi
+		fmt.Printf("String: %s \n", "Hello")
+		fmt.Printf("Tirnak içinde: %q \n", "Hello")
+		fmt.Printf("Hex: %x\n", "Range Over Iterators")
 
-	// String formatlama ve Pointer adresi
-	fmt.Printf("String: %s \n", "Hello")
-	fmt.Printf("Tirnak içinde: %q \n", "Hello")
-	fmt.Printf("Hex: %x\n", "Range Over Iterators")
+		x := 9
+		var num int = 42
+		fmt.Printf("Pointer: %p\n", &num)
+		fmt.Printf("pointer:%p\n", &x)
+	*/
 
-	var num int = 42
-	fmt.Printf("Pointer: %p\n", &num)
+	// Width & Precision
+
+	fmt.Printf("Saga hizala: %6d %6d\n", 12, 345)
+	fmt.Printf("Float (2 ondalık): |%6.2f|%6.2f|\n", 1.2, 3.45)
+	fmt.Printf("Sol hizali: |%-6.2f|%-6.2f|\n", 1.2, 3.45)
+	fmt.Printf("Metin saga hizali:|%6s|%6s|\n", "foo", "b")
+	fmt.Printf("Metin sola hizali:|%-6s|%-6s|\n", "foo", "b")
+
+	fmt.Printf("6 karakterlik alan, saga kaydir.: %6s %6s", "good", "luck")
 }
